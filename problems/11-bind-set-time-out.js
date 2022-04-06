7fad6a4d402ba6a94dd34d401d1e6f4d858d58e8
Irving Arreola Palacios <irving.a789@gmail.com>
2022-04-06 15:54:27 -0500

    11

--- problems/11-bind-set-time-out.js
+++ problems/11-bind-set-time-out.js
@@ -1,7 +1,30 @@
 function boundFuncTimer(obj, func, delay) {
   // Your code here
+  let boundFunc =func.bind(obj);
+  setTimeout(()=>{
+    return boundFunc();
+  }, delay);
 }
 
+
+
+
+class Animal {
+  constructor(age) {
+    this.age = age;
+  }
+
+  growOlder() {
+    this.age++;
+    console.log(this.age);
+  }
+}
+
+const dog = new Animal(1);
+const cat = new Animal(5);
+
+console.log(boundFuncTimer(dog, dog.growOlder, 5000)); // in 5 seconds prints: 2
+boundFuncTimer(cat, dog.growOlder, 7000); // in 7 seconds prints: 6
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
